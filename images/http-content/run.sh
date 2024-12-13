a4f50081e50e09dbd96be76ef8fe5e4531c186b4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-13 14:18:30 +0530

    image changed

--- images/http-content/run.sh
+++ images/http-content/run.sh
@@ -1,1 +1,44 @@
-/usr/share/nginx/html
+if [ -z "$app_name" ]; then
+  echo app_name is missing
+fi
+if [ -z "$env" ]; then
+  echo env is missing
+fi
+
+
+
+apiVersion: v1
+kind: Pod
+metadata:
+ name: pod-01
+spec:
+ initContainers:
+ - name: pod1
+   image: busybox
+   env:
+   - name: "DB_HOST"
+     value: "SQL"
+     volumeMounts:
+     - name: image-path
+       mountPath: /data
+
+ volumes:
+ - name: image-path
+   emptyDir: {}
+
+ containers:
+ - name: cont
+   image: busybox
+   volumeMounts:
+   - name: image-path
+     mountPath: /data
+
+     FROM centos
+     COPY /run.sh
+     ENTRYPOINT ["bash","/run.sh"]
+
+     run.sh
+     ======
+     if [-z "$DB_HOST"]; then
+       echo missing
+     fi
